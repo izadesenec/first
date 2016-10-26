7e89c8a195ccd9ef1ca35a944d5a491d0c6cab1e
max <max@max>
2016-10-26 20:18:54 +0300

    start page of site and nb to gitignore list

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo '<h1>Hello WORLD!!!</h1>';
+        echo '<img width="75%" src="https://enotcorp.org/wp-content/uploads/2015/12/siski-turciya-azer.jpg">';
+        ?>
+    </body>
+</html>
